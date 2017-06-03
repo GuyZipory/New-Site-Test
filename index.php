55b3a2950dfa01aaf3f482003a0de29aabc31ce0
Guy Zipory <guyzip@gmail.com>
2017-06-03 19:25:47 +0300

    Intractive index
    Welcome massage base of the user who log in

--- index.php
+++ index.php
@@ -1,8 +1,24 @@
 
-
+<?php
+include 'dbh.php';
+session_start();
+if (!$_SESSION["user"])
+{
+header("Location: login.php");
+}else{
+$user = $_SESSION["user"];
+}
+ ?>
 <html>
 <title>New PHP Test Site</title>
   <body>
-<center><h1> welcome!</h1></center>
+    <?php
+      $query = "SELECT * FROM users WHERE username='$user'";
+      $result = mysqli_query($mysql, $query);
+      $rr = mysqli_fetch_assoc($result);
+      echo "Welcome ".$rr['firstname']." " .$rr['lastname']."<br>";
+     ?>
+  <form action="logout.php" method="post">
+    <button type="submit">Logout!</button><br>
   </body>
 </html>
