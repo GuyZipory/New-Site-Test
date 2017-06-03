8c48a49718864dc64c6ddf950c655c9ab6a99a11
Guy Zipory <guyzip@gmail.com>
2017-06-03 15:10:55 +0300

    Basic SignUP
    The basix form of the signup.php

---
+++ signup.php
@@ -0,0 +1,48 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>Sign Up</title>
+</head>
+<body>
+<form action="signup.php" method="post">
+  <input type="text" name="user" placeholder="Username"><br>
+  <input type="text" name="first" placeholder="First Name"><br>
+  <input type="text" name="last" placeholder="Last Name"><br>
+  <input type="password" name="pwd" placeholder="Password"><br>
+  <select name="gender">
+    <option value="male">Male</option>
+    <option value="female">Female</option>
+  </select><br>
+  <button type="submit">Sign Up!</button><br>
+  <?php
+  include 'dbh.php';
+  if(isset($_POST['gender']))
+  {
+  $user = $_POST['user'];
+  $first = $_POST['first'];
+  $last = $_POST['last'];
+  $pwd = $_POST['pwd'];
+  $gender = $_POST['gender'];
+
+  if (strlen($user) < 3)
+{
+  echo "Username have to have at list 3 Characters!";
+}   elseif (!$first) {
+    echo "You must enter First Name!";
+}     elseif (!$last) {
+      echo "You must enter Last Name!";
+}       elseif (!$pwd) {
+        echo "You must enter Password!";
+}         elseif (strlen($pwd) < 6) {
+          echo "The Password must be at list 6 Characters!";
+}else {
+  $query = "INSERT INTO users (username, firstname, lastname, password, gender)
+  VALUES ('$user', '$first' , '$last', '$pwd' , '$gender')";
+  $result = mysqli_query($mysql, $query);
+  header("Location: index.php");
+}
+  }
+   ?>
+</body>
