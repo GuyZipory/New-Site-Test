d436b0966fc04029c23e4bc7a1b908282f535931
Guy Zipory <guyzip@gmail.com>
2017-06-03 15:10:16 +0300

    change Index.php to basic design
    deleted all PHP testing the the index

--- index.php
+++ index.php
@@ -1,24 +1,8 @@
-<?php
-// Database connection
-$dbhost = "localhost";
-$dbuser = "guyzipory";
-$dbpass = "3MLU8rljIah8iE3o";
-$dbname = "phptest";
-$mysql = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
-//Test connection
-if(mysqli_connect_errno()) {
-  die("Database connection Failed: " .
-    mysqli_connect_error() .
-    " (" . mysqli_connect_errno() . ")"
-  );
-}
 
 
- ?>
 <html>
 <title>New PHP Test Site</title>
+  <body>
+<center><h1> welcome!</h1></center>
+  </body>
 </html>
-<?php
-//Close Database connection
-mysqli_close($mysql);
- ?>
