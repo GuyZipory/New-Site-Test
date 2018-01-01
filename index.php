bf3cf023bdb78f1c2e542cd8567e50f773ffa695
Guy Zipory <guyzip@gmail.com>
2018-01-01 16:21:30 +0200

    delete !
    delete ! for welcome

--- index.php
+++ index.php
@@ -16,7 +16,7 @@ $user = $_SESSION["user"];
       $query = "SELECT * FROM users WHERE username='$user'";
       $result = mysqli_query($mysql, $query);
       $rr = mysqli_fetch_assoc($result);
-      echo "Welcome! ".$rr['firstname']." " .$rr['lastname']."<br>";
+      echo "Welcome ".$rr['firstname']." " .$rr['lastname']."<br>";
      ?>
   <form action="logout.php" method="post">
     <button type="submit">Logout!</button><br>
